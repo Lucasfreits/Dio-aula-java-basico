e9b19ab0c4cd5942ff83a8afe7b69fe5bd79f8dd
projetodio <thibarox@gmail.com>
2024-05-22 10:03:34 -0300

    minha classe

---
+++ java-anatomia-classes/src/MinhaClasse.java
@@ -0,0 +1,10 @@
+public class MinhaClasse {
+
+public static void main (String [] args) {
+
+    System.out.print("Estou aprendo Java básico, com a bolsa que ganhei da DIO, saindo do absoluto zero, para tentar entender , aprender e quem sabe trabalhar na área.");
+    
+
+}
+
+}
